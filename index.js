46246211bf0a002d5241230d25db4b3c4a5c10a8
Roy Koigu <roykoigu@protonmail.com>
2025-07-17 03:49:48 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // RETURN the fetch promise!
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Could not connect: ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      renderBooks(data);
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerText = book.name;    // innerText is safer for plain text
     main.appendChild(h2);
   });
 }
